df03f54607c07f58ff9f17b35b632fe67d2c5000
GracieKYS <117329856+GracieKYS@users.noreply.github.com>
2023-04-11 12:50:21 +0800

    second day

--- main.js
+++ main.js
@@ -6,15 +6,34 @@ const submitButton = document.getElementById('submit_button');
 let nameArr = []
 let ageArr = []
 
+
+
 function clickHandler(e) {
   e.preventDefault();
   let formData = new FormData(form);
 
   nameArr.push(formData.get('name'));
   ageArr.push(formData.get('age'));
-  
 
+//  viewNameArr();
+//  viewAgeArr();
 }
 
-
 submitButton.addEventListener('click', clickHandler);
+
+function viewNameArr() {
+
+  for(let i = 0; i < nameArr.length; i++) {
+    console.log(nameArr[i])
+  }
+}
+
+function viewAgeArr() {
+
+  for(let i = 0; i < ageArr.length; i++) {
+    nameArr.forEach((age)=> {
+      console.log(age)
+    })
+  }
+
+}
